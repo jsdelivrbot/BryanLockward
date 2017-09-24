c0ef1c3e0caa2ad5bf2d85933df0c8b34cce541f
Bryan Lockward <bmlockward3@verizon.net>
2017-09-23 21:55:36 -0400

    Changed Session

--- Express_Lockward/counter/server.js
+++ Express_Lockward/counter/server.js
@@ -2,28 +2,36 @@ const express = require("express");
 const app = express();
 const path=require("path");
 const bodyParser=require("body-parser")
+const session = require('express-session');
 var port=process.env.PORT || 8000;
-
+app.use(session({secret:'secret'}));
 app.use(bodyParser.urlencoded({extended : true}));
 
-var count=0;
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 
+
 app.get('/',function(request,response){
-  count+=1;
-  response.render('index',{count:count});
+  if(request.session.count)
+  {
+    request.session.count++;
+  }
+  else{
+    request.session.count=1;
+  }
+  var counter=request.session.count
+  response.render('index',{counter});
 
 });
 
 app.post('/reset',function(request,response){
-  count=0;
+  request.session.count=0;
   response.redirect('/');
 });
 
 app.post('/plus2',function(request,response){
-  count+=1;
+  request.session.count+=1;
   response.redirect('/');
 })
 
